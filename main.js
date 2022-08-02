47957e505c589d87d59b3dba50dd60ae58e1f69a
Yo <taphanel.juan@gmail.com>
2022-08-02 20:22:35 -0300

    Arreglo con funcion

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
 let aviso = alert("Usa Mayusculas y si te cansas escribi ESC");
-let entrada =  prompt("Cuales son las 5 mejores ciudades segun TIME OUT").toLowerCase();
 
-while (entrada != 'ESC') {
-    switch (entrada) {
-        case "edimburgo":
-            alert("Puesto Nº1");
-            break;
-        case "chicago":
-            alert("Puesto Nº2");
-            break;
-        case "medellin":
-            alert("Puesto Nº3");
-            break;
-        case "glasgow":
-            alert("Puesto Nº4");
-            break;
-        case "amsterdam":
-            alert("Puesto Nº5");
-            break;
-        default:
-            alert("No esta en el TOP 5");
-            break;
-    }
-    entrada = prompt("Cuales son las 5 mejores ciudades segun TIME OUT");
+function entradaGrosa() {
+  return prompt("Cuales son las 5 mejores ciudades segun TIME OUT").toLowerCase();
+}
+
+let entrada = entradaGrosa();
+
+while (entrada != "ESC") {
+  switch (entrada) {
+    case "edimburgo":
+      alert("Puesto Nº1");
+      break;
+    case "chicago":
+      alert("Puesto Nº2");
+      console.log(entrada);
+      break;
+    case "medellin":
+      alert("Puesto Nº3");
+      break;
+    case "glasgow":
+      alert("Puesto Nº4");
+      break;
+    case "amsterdam":
+      alert("Puesto Nº5");
+      break;
+    default:
+      alert("No esta en el TOP 5");
+      break;
+  }
+  entrada = entradaGrosa();
 }
